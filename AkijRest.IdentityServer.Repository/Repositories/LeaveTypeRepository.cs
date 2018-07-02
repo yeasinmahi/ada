ea1ea059199caadb76589b4d9b98ab383c764a5e
arafat.corp <arafat.corp@AGLAPTOP-191>
2018-07-02 15:08:28 +0600

    add some method to Leavetyperepository

--- AkijRest.IdentityServer.Repository/Repositories/LeaveTypeRepository.cs
+++ AkijRest.IdentityServer.Repository/Repositories/LeaveTypeRepository.cs
@@ -9,28 +9,41 @@ using AkijRest.IdentityServer.Repository.Models;
 
 namespace AkijRest.IdentityServer.Repository.Repositories
 {
-    public class LeaveTypeRepository: ILeaveTypeRepository
+    public class LeaveTypeRepository : ILeaveTypeRepository
     {
-        private IdentityServerDbContext context;
+        private readonly IdentityServerDbContext _context;
         public LeaveTypeRepository()
         {
-            this.context = new IdentityServerDbContext();
+            this._context = new IdentityServerDbContext();
         }
 
         public List<LeaveTypeDto> Get()
         {
             List<LeaveTypeDto> listLeaveTypeDto = new List<LeaveTypeDto>();
 
-            if (context != null)
+            if (_context != null)
             {
-                var leaveTypes = context.LeaveTypes.ToList();
+                var leaveTypes = _context.LeaveTypes.ToList();
 
                 foreach (var leaveType in leaveTypes)
                 {
-                    LeaveTypeDto dto = new LeaveTypeDto();
+                    LeaveTypeDto dto = new LeaveTypeDto
+                    {
+                        Id = leaveType.Id,
+                        Name = leaveType.Name,
+                        ApplicationFor = leaveType.ApplicationFor,
+                        CompanyPolicy = leaveType.CompanyPolicy,
+                        IsBalanceChecked = leaveType.IsBalanceChecked,
+                        IsHalfDayAllowed = leaveType.IsHalfDayAllowed,
+                        IsOnlyOneTime = leaveType.IsOnlyOneTime,
+                        IsRestricted = leaveType.IsRestricted,
+                        MaxApplicationAtAMonth = leaveType.MaxApplicationAtAMonth,
+                        MaximumAllowedAtATime = leaveType.MaximumAllowedAtATime
 
-                    dto.Name = leaveType.Name;
-                    dto.Id = leaveType.Id;
+
+
+                    };
+
 
                     listLeaveTypeDto.Add(dto);
                 }
@@ -40,49 +53,54 @@ namespace AkijRest.IdentityServer.Repository.Repositories
 
             throw new Exception();
         }
-        //public LeaveDto Create(LeaveDto leaveDto)
-        //{
-        //    DateTime dateTime
-        //        = Global.Datetime.ToDateTime(leaveDto.DateStart);
+        public LeaveTypeDto Get(int id)
+        {
+            if (_context != null)
+            {
+                LeaveType leaveType = _context.LeaveTypes.FirstOrDefault(x=>x.Id.Equals(id));
+                LeaveTypeDto dto = null;
+                if (leaveType != null)
+                {
+                    dto = new LeaveTypeDto
+                    {
+                        Id = leaveType.Id,
+                        Name = leaveType.Name,
+                        ApplicationFor = leaveType.ApplicationFor,
+                        CompanyPolicy = leaveType.CompanyPolicy,
+                        IsBalanceChecked = leaveType.IsBalanceChecked,
+                        IsHalfDayAllowed = leaveType.IsHalfDayAllowed,
+                        IsOnlyOneTime = leaveType.IsOnlyOneTime,
+                        IsRestricted = leaveType.IsRestricted,
+                        MaxApplicationAtAMonth = leaveType.MaxApplicationAtAMonth,
+                        MaximumAllowedAtATime = leaveType.MaximumAllowedAtATime
+                    };
 
-        //    DateTime dateTimeEnd
-        //        = Global.Datetime.ToDateTime(leaveDto.DateEnd);
+                }
+                return dto;
+            }
+            throw new Exception();
+        }
+        public int Create(LeaveTypeDto leaveTypeDto)
+        {
+            LeaveType leaveType = new LeaveType
+            {
+                ApplicationFor = leaveTypeDto.ApplicationFor,
+                CompanyPolicy = leaveTypeDto.CompanyPolicy,
+                IsBalanceChecked = leaveTypeDto.IsBalanceChecked,
+                IsHalfDayAllowed = leaveTypeDto.IsHalfDayAllowed,
+                IsOnlyOneTime = leaveTypeDto.IsOnlyOneTime,
+                IsRestricted = leaveTypeDto.IsRestricted,
+                MaxApplicationAtAMonth = leaveTypeDto.MaxApplicationAtAMonth,
+                MaximumAllowedAtATime = leaveTypeDto.MaximumAllowedAtATime,
+                Name = leaveTypeDto.Name
 
+            };
+            _context.LeaveTypes.Add(leaveType);
+            _context.SaveChanges();
 
-        //    for (; ; )
-        //    {
-        //        var leave = new Leave
-        //        {
-        //            LeaveTypeId = leaveDto.LeaveTypeId,
-
-        //            UserId = context
-        //                .Users
-        //                .SingleOrDefault
-        //                (
-        //                    u =>
-        //                        u.UserName.Equals(leaveDto.UserName)
-        //                ).Id,
-
-        //            Date = dateTime,
-
-        //            LeaveCause = leaveDto.LeaveCause,
-        //            LeaveAddress = leaveDto.LeaveAddress
-        //        };
-
-        //        context.Leaves.Add(leave);
-
-        //        dateTime = dateTime.AddDays(1);
-
-        //        if (dateTime > dateTimeEnd)
-        //        {
-        //            break;
-        //        }
-        //    }
-
-        //    context.SaveChanges();
-
-        //    return (leaveDto);
-        //}
+            return (leaveType.Id);
+        }
+        
 
     }
 }
