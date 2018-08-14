b2ffc263501b6f42d808ffd4fd217303c7011177
arafat.corp <arafat.corp@akij.net>
2018-08-14 17:48:01 +0600

    published to agvdi4

--- Demo.MenuLoad/Controllers/Scripts/StaticContent.js
+++ Demo.MenuLoad/Controllers/Scripts/StaticContent.js
@@ -1,6 +1,6 @@
 ﻿//Auth related URL
-//var tokenUrlPrefix = "https://agvdi4.akij.net:44321/api";
-var tokenUrlPrefix = "https://localhost:44321/api";
+var tokenUrlPrefix = "https://agvdi4.akij.net:44321/api";
+//var tokenUrlPrefix = "https://localhost:44321/api";
 var facebookAuth = tokenUrlPrefix + "/userAuth/facebook/";
 var gmailAuth = tokenUrlPrefix + "/userAuth/gmail/";
 var tokenUrl = tokenUrlPrefix + "/token";
@@ -10,8 +10,8 @@ var refreshUrl = tokenUrl + "/refresh";
 var rolesUrl = tokenUrl + "/roles";
 
 //All Api related URL
-//var apiUrlPrefix = "https://agvdi4.akij.net:44386/api";
-var apiUrlPrefix = "https://localhost:44386/api";
+var apiUrlPrefix = "https://agvdi4.akij.net:44386/api";
+//var apiUrlPrefix = "https://localhost:44386/api";
 var userUrl = apiUrlPrefix + "/users";
 var leavetypeUrl = apiUrlPrefix + "/leavetype";
 var leaveUrl = apiUrlPrefix + "/leaves";
