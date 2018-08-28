35e0e263e912df0300f41d98afc8450699c4a040
arafat.corp <arafat.corp@akij.net>
2018-08-28 15:58:44 +0600

    nothing important

--- AkijRest.IdentityServer.Repository/Dtos/RoleGroupDto.cs
+++ AkijRest.IdentityServer.Repository/Dtos/RoleGroupDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AkijRest.IdentityServer.Repository.Dtos
+﻿namespace AkijRest.IdentityServer.Repository.Dtos
 {
     public class RoleGroupDto
     {
